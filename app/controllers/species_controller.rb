ca8aa2f74385b4cfd351668408554021452d6c5b
Randy Leighton & Tanner Steward <student@epicodus.com>
2014-08-26 12:20:53 -0700

    add destroy - 7 RESTful actions complete

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -32,4 +32,10 @@ class SpeciesController < ApplicationController
       render('species/edit.html.erb')
     end
   end
+
+  def destroy
+    @species = Species.find(params[:id])
+    @species.destroy
+    render('species/destroy.html.erb')
+  end
 end
