98714830fe1561ae32fc0ce109fed0cbbba25840
Randy Leighton & Tanner Steward <student@epicodus.com>
2014-08-27 16:18:03 -0700

    add search for sighting in a given date range in model

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,6 +1,10 @@
 class Sighting < ActiveRecord::Base
-validates :date, :presence => true
-belongs_to :species
-belongs_to :region
+  validates :date, :presence => true
+  belongs_to :species
+  belongs_to :region
 
+  def self.date_search(start_date, end_date)
+    search_result = self.where(date: start_date..end_date)
+    search_result
+  end
 end
--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -4,4 +4,12 @@ describe Sighting do
   it {should belong_to :species}
   it {should validate_presence_of :date}
   it {should belong_to :region}
+
+  it "should find all sightings in a date range" do
+    yogi_bear = Species.create({name: "Yogi Bear"})
+    sighting1 = Sighting.create(species_id: yogi_bear.id, date: "2014-08-11")
+    sighting2 = Sighting.create(species_id: yogi_bear.id, date: "2014-08-14")
+    sighting3 = Sighting.create(species_id: yogi_bear.id, date: "2014-08-26")
+    expect(Sighting.date_search("2014-08-10", "2014-08-25")).to eq [sighting1, sighting2]
+  end
 end
