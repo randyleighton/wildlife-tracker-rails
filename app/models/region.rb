c1460accd28eacbbb1f5dd53e2e11c13e180a0d1
Randy Leighton & Tanner Steward <student@epicodus.com>
2014-08-27 10:16:22 -0700

    region has many species through sightings

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,4 +2,5 @@ class Region < ActiveRecord::Base
 
   validates :name, presence: true
   has_many :sightings
+  has_many :species, through: :sightings
 end
--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Region do
   it {should validate_presence_of :name}
   it {should have_many :sightings}
+  it {should have_many(:species).through(:sightings)}
 end
