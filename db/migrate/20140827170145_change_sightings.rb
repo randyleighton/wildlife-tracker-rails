146aee7286c54cc6daf5412b4aca4d547e8a5d0d
Randy Leighton & Tanner Steward <student@epicodus.com>
2014-08-27 10:02:48 -0700

    modify sightings table by adding a region id

---
+++ db/migrate/20140827170145_change_sightings.rb
@@ -0,0 +1,5 @@
+class ChangeSightings < ActiveRecord::Migration
+  def change
+    add_column :sightings, :region_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827170014) do
+ActiveRecord::Schema.define(version: 20140827170145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140827170014) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "species_id"
+    t.integer  "region_id"
   end
 
   create_table "species", force: true do |t|
